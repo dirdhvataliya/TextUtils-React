ad8e19454728501b655eaeda10184b6b1e967803
dirdhvataliya <dirdhvataliya2003@gmail.com>
2024-06-20 22:39:03 +0530

    Updates

---
+++ precache-manifest.7b4531931401fd493849cc3324120d80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6066ae90acd3e98aeb6392a30cd0c7",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "2e7d829c82658592940f",
+    "url": "/TextUtils-React/static/css/main.f0cb387e.chunk.css"
+  },
+  {
+    "revision": "e67754bdf9ffbb057920",
+    "url": "/TextUtils-React/static/js/2.af68fada.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.af68fada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/TextUtils-React/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "2e7d829c82658592940f",
+    "url": "/TextUtils-React/static/js/main.6eea059c.chunk.js"
+  },
+  {
+    "revision": "ebec0e7e4c807e308fc4",
+    "url": "/TextUtils-React/static/js/runtime-main.93eafebd.js"
+  }
+]);
